8ff8e593b836ea9838b995bd135ac6a71d3bc8b9
HanHor <hanhor.wu@gmail.com>
2012-12-03 16:43:15 +0800

    test on appfog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Seearm::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   # 在APPFOG测试用改为true,default false
-  config.server_static_assets = true
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
