cafe752fc612ff7fd15de3e7b798ab571e2d61c2
HanHor <hanhor.wu@gmail.com>
2012-12-05 11:18:45 +0800

    production test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Seearm::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  #config.assets.precompile += %w(app.js cpanel.js seearm.js fontawesome.css seearm-responsive.css seearm.css application cpanel)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
