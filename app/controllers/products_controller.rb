156aafab8288a4b4ac1c18ba816fa51327370544
HanHor <hanhor.wu@gmail.com>
2012-12-10 13:37:45 +0800

    products details page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,9 +2,5 @@ class ProductsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @product }
-    end
   end
 end
