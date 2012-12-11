8ac7057e034c85a9d091184fa385bb8c6f462a55
HanHor <hanhor.wu@gmail.com>
2012-12-11 10:11:55 +0800

    categories

--- db/migrate/20121120073810_create_categories.rb
+++ db/migrate/20121120073810_create_categories.rb
@@ -3,7 +3,7 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.string :name
       t.string :slug
-      t.boolean :type
+      t.boolean :type #分类类型：新闻分类，产品分类共用一张表。通过type字段区分是什么分类。
       t.text :desc
 
       t.timestamps
