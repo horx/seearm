0f0964df256b3ca5855e109d0bc363552077a6fc
HanHor <hanhor.wu@gmail.com>
2012-12-11 14:47:09 +0800

    add errors(404,403,500) pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # coding: utf-8
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def render_404
+    render_optional_error_file(404)
+  end
+
+  def render_403
+    render_optional_error_file(403)
+  end
+
+  def render_optional_error_file(status_code)
+    status = status_code.to_s
+    if ["404","403", "422", "500"].include?(status)
+      render :template => "/errors/#{status}", :format => [:html], :handler => [:erb], :status => status, :layout => "application"
+    else
+      render :template => "/errors/unknown", :format => [:html], :handler => [:erb], :status => status, :layout => "application"
+    end
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,18 @@
 # coding: utf-8
 class CategoriesController < ApplicationController
-  #before_filter :find_cat, :only => [:show]
+  before_filter :find_category, :only => [:show]
   def show
-  	@cat = Category.where(:slug => params[:id])
-    render_404 if @cat.nil?
   end
 
   protected
-  def find_cat
+  def find_category
     # 处理有大写字母的情况
     if params[:id] != params[:id].downcase
       redirect_to request.path.downcase, :status => 301
       return
     end
-    @cat = Category.where(:slug => /^#{params[:id]}$/i)
-    render_404 if @cat.nil?
+    @category = Category.where(:slug => params[:id]).first
+    render_404 if @category.nil?
   end
+
 end
