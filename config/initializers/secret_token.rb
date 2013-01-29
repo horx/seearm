9e1aaa338d8a72fd734b526bc5f6ab79c4a30633
HanHor <hanhor.wu@gmail.com>
2013-01-29 10:14:19 +0800

    modified:   config/initializers/secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Seearm::Application.config.secret_token = '60edf7633666de918a2fb60e5b0b6ea7654790349024269366b01ccd04890c254a0fa2b347f3d0c93ef56ba2b8275329304fbc1784c59d547464ec4af82c19d6'
+Seearm::Application.config.secret_token = '4843dc16608098bdc9cf336fb794f61fc06c1b1f9acadd1455c99036ebf7b1fb7cc11b8ad0e25debd834639186d6e03da3c954eef7f55d622c3aa8970d7dc638'
