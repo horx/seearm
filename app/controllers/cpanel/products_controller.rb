cbeaf35284865cbca2ab40c98d07a3c1d3340364
HanHor <hanhor.wu@gmail.com>
2013-02-18 14:10:41 +0800

    update gems

--- app/controllers/cpanel/products_controller.rb
+++ app/controllers/cpanel/products_controller.rb
@@ -2,8 +2,8 @@
 class Cpanel::ProductsController < Cpanel::ApplicationController
   before_filter :find_product, :only => [ :edit, :update, :destroy]
   def index
-  	#@products = Product.paginate( :page => params[:page], :per_page => 15 )
-    @products = Product.all
+  	@products = Product.paginate( :page => params[:page], :per_page => 15 )
+    #@products = Product.all
   end
 
   def new
