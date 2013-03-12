cc491b9d54ee76447633ad360a3fa2711447842d
HanHor <hanhor.wu@gmail.com>
2013-03-12 13:08:29 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,7 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
-  #after "deploy:finalize_update", "deploy:symlink_config"
-  before "bundle:install", "deploy:symlink_config"
+  after "deploy:finalize_update", "deploy:symlink_config"
 
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
