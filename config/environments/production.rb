853cc2d9df419b30af4bf7971863a48458d14c0d
HorX <hanhor.wu@gmail.com>
2013-05-15 21:57:17 +0800

    fix auth 5xx bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,5 @@ Seearm::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => Setting.domain }
+  config.action_mailer.default_url_options = { :host => 'new.seearm.com:3000' }
 end
