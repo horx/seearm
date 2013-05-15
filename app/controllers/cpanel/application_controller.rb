f8a3445dc262f5401bc00f3a4dc1df7437de2125
HorX <hanhor.wu@gmail.com>
2013-05-15 21:46:00 +0800

    fix auth 5xx bug

--- app/controllers/cpanel/application_controller.rb
+++ app/controllers/cpanel/application_controller.rb
@@ -2,7 +2,7 @@
 class Cpanel::ApplicationController < ApplicationController
   layout "cpanel"
   before_filter :require_user
-  #before_filter :require_admin
+  before_filter :require_admin
 
   def require_admin
     if not Setting.admin_emails.include?(current_user.email)
