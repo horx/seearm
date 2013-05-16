08c43b2e0d9279d2aaf7e01cdc83f20be971ca1f
HorX <hanhor.wu@gmail.com>
2013-05-16 09:29:53 +0800

    test devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,20 +65,21 @@ Seearm::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+
   config.action_mailer.default_url_options = { :host => 'new.seearm.com' }
+  config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default :charset => "utf-8"
 
-  config.action_mailer.smtp_settings = {
-      address: "smtp.gmail.com",
-      port: 587,
-      domain: "new.seearm.com",
-      authentication: "plain",
-      enable_starttls_auto: true,
-      user_name: "usale.it@gmail.com",
-      password: "15000288966h"
+  ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :enable_starttls_auto => true,
+    :port => 587,
+    :authentication => :plain,
+    :user_name => "usale.it@gmail.com",
+    :password => '15000288966h'
   }
+
+  config.action_mailer.default_url_options = { :host => 'new.seearm.com' }
+
 end
