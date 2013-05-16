cd648b3c85ff0498ed98f71f4448ca023440cd53
HorX <hanhor.wu@gmail.com>
2013-05-16 13:04:34 +0800

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Seearm::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
@@ -66,17 +66,17 @@ Seearm::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
 
-  config.action_mailer.default_url_options = { :host => 'new.seearm.com' }
+  config.action_mailer.default_url_options = { :host => Setting.domain }
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :address => "smtp.gmail.com",
+    :address => Setting.email_server,
     :enable_starttls_auto => true,
     :port => 587,
     :authentication => :plain,
-    :user_name => "usale.it@gmail.com",
-    :password => '15000288966h'
+    :user_name => Setting.email_sender,
+    :password => Setting.email_password
   }
 end
