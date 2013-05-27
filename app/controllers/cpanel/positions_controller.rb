8d5b1e950bd92ba2fc9164992388404ec602a73f
HorX <hanhor.wu@gmail.com>
2013-05-27 12:17:46 +0800

    positions curd

--- app/controllers/cpanel/positions_controller.rb
+++ app/controllers/cpanel/positions_controller.rb
@@ -1,2 +1,46 @@
-class Cpanel::PositionsController < ApplicationController
+# coding: utf-8
+class Cpanel::PositionsController < Cpanel::ApplicationController
+    before_filter :find_position, :only => [ :edit, :update, :destroy]
+
+    def index
+      @positions = Position.paginate( :page => params[:page], :per_page => 15 )
+    end
+
+    def new
+      @product = Position.new
+    end
+
+    def create
+      @position = Position.new(params[:position])
+      if @position.save
+        redirect_to :action => :index
+      else
+        render :action => :new
+      end
+      flash[:notice] = "推荐内存创建成功"
+    end
+
+    def edit
+    end
+
+    def update
+      if @position.update_attributes(params[:position])
+        redirect_to cpanel_positions_path
+      else
+        render :action => :edit
+      end
+      flash[:notice] = "推荐内容更新成功"
+    end
+
+    def destroy
+      @position.destroy
+      redirect_to :action => :index
+      flash[:alert] = "推荐内容已被删除"
+    end
+
+    private
+    def find_position
+      @position = Position.find(params[:id])
+    end
+
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -1,3 +1,3 @@
 class Position < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :title, :desc, :image, :url
 end
