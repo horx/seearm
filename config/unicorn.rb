11943889cd3c1c414fd9cc2388974f2d84070c40
HorX <hanhor.wu@gmail.com>
2013-05-29 14:28:54 +0800

    unicorn log

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -5,8 +5,8 @@ end
 root = "/home/seeapp/apps/seearm/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/err_unicorn.log"
+stdout_path "#{root}/log/out_unicorn.log"
 
 listen "/tmp/unicorn.seearm.sock"
 worker_processes 2
