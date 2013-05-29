9a465fa37641ed6a406f3e41ba4ec135e5264735
HorX <hanhor.wu@gmail.com>
2013-05-29 13:34:50 +0800

    categoried tree

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,25 +1,37 @@
+# coding: utf-8
 module CategoriesHelper
 
   def display_categories(categories)
-    tree = '<ul class="top">'
+    tree = "<ul class='nav nav-list'>"
     for category in categories
       if category.parent_id == 0
-        tree += '<li class="nav-header">'
-        tree += link_to category.name
+        tree += "<li class='nav-header'>"
+        tree += category.name
         tree += find_all_subcategories(category)
-        tree += '</li>'
-
+        tree += "</li>"
       end
     end
-    tree += '</ul>'
+    tree += "</ul>"
+    return tree.html_safe
   end
 
   def find_all_subcategories(category)
+    if category.children.size > 0
+      tree = ''
       category.children.each { |subcat|
+        if subcat.children.size > 0
+          tree += '<li>'
+          tree += link_to h(subcat.name), :action => 'show', :id => subcat
+          tree += find_all_subcategories(subcat)
+          tree += '</li>'
+        else
           tree += '<li>'
           tree += link_to h(subcat.name), :action => 'show', :id => subcat
           tree += '</li>'
+        end
       }
+      tree += ''
+    end
   end
 
 end
