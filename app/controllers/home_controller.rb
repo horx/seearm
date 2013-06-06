498394b48b9ced0731cdacf2b7017e89a8fa8881
HorX <hanhor.wu@gmail.com>
2013-06-06 15:35:35 +0800

    fix home page display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,9 @@ class HomeController < ApplicationController
   
   def index
     @on = 'home'
-    @position_one = Position.where('location = 1').limit(4).order('updated_at DESC')
-    @position_two = Position.where('location = 3').limit(4).order('updated_at DESC')
-    @position_three = Position.where('location = 3').limit(4).order('updated_at DESC')
+    @position_one = Position.where('location = 1').order('updated_at DESC')
+    @position_two = Position.where('location = 2').limit(8).order('updated_at DESC')
+    @position_three = Position.where('location = 3').limit(8).order('updated_at DESC')
   end
 
 end
